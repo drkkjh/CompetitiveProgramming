0f5aa570f9c2eda9f7d5f01b48545662773556e9
drkkjh <61372278+drkkjh@users.noreply.github.com>
2024-02-18 14:31:07 -0500

    Add template for cp problems with fast I/O

---
+++ template.cpp
@@ -0,0 +1,16 @@
+// Author: Derrick Khoo
+// It is ok to share my code anonymously for educational purposes.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+
+    cout << varName << "\n";
+};
